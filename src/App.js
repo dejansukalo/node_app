4a68af5d38b02773f0bd2ff7cae8e64f1fce673a
dejansukalo <dejan.sukalo@gmail.com>
2022-05-20 09:19:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gdje si majstore?
         </p>
         <a
           className="App-link"
